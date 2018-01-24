f493f90d9f6b4f803b637bd1a8f672e5c97d6bf6
DurrainZehra <durrain.453@hotmail.com>
2018-01-24 08:36:50 -0800

    Initial commit

---
+++ app/src/main/java/com/example/point/myapplication/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.point.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn= (Button) findViewById(R.id.mynewbutton); btn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override public void onClick(View v)
+            { Toast.makeText(MainActivity.this,"Welcome to android",Toast.LENGTH_LONG).show();
+            Intent intent=new Intent(MainActivity.this,SecondActivity.class);
+            startActivity(intent);
+
+        } });
+    }
+}
